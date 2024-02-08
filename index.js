f40992b7540a415fd773dc3cca41b701fbb1acfd
Almoali <almoali399@outlook.com>
2024-02-09 00:48:34 +0200

    using functions and espesially "parameters" to improve the RPS Game

--- index.js
+++ index.js
@@ -1,71 +1,50 @@
-let computerMove = '';
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
+    let result = '';
+    if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
+            result = 'You lose';
+        } else if (computerMove === 'paper') {
+            result = 'You win';
+        } else if (computerMove === 'scissors') {
+            result = 'Tie';
+        }
+    } else if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
+            result = 'You win';
+        } else if (computerMove === 'paper') {
+            result = 'Tie';
+        } else if (computerMove === 'scissors') {
+            result = 'You lose';
+        }
+    } else if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie';
+        } else if (computerMove === 'paper') {
+            result = 'You lose';
+        } else if (computerMove === 'scissors') {
+            result = 'You win';
+        }
+    }
+    // Update the result in the corresponding div
+    if (playerMove === 'rock') {
+        document.getElementById('result').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}. ${result}`;
+    } else if (playerMove === 'paper') {
+        document.getElementById('result2').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}. ${result}`;
+    } else if (playerMove === 'scissors') {
+        document.getElementById('result3').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}. ${result}`;
+    }
+}
 
 function pickComputerMove() {
+    let computerMove = '';
     const randomNumber = Math.random();
     if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = 'Rock';
+        computerMove = 'rock';
     } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = 'Paper';
+        computerMove = 'paper';
     } else {
-        computerMove = 'Scissors';
+        computerMove = 'scissors';
     }
-
-    let result = '';
-    if (computerMove === 'Rock') {
-        result = 'tie';
-    } else if (computerMove === 'Paper') {
-        result = 'you lose';
-    } else {
-        result = 'you win';
-    }
-
-    document.getElementById('result').innerHTML = `You picked Rock. Computer picked ${computerMove}. ${result}.`;
+    return computerMove;
 }
-
-function pickComputerMove2() {
-    const randomNumber = Math.random();
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = 'Rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = 'Paper';
-    } else {
-        computerMove = 'Scissors';
-    }
-
-    let result = '';
-    if (computerMove === 'Rock') {
-        result = 'you win';
-    } else if (computerMove === 'Paper') {
-        result = 'tie';
-    } else {
-        result = 'you lose';
-    }
-
-    document.getElementById('result2').innerHTML = `You picked Paper. Computer picked ${computerMove}. ${result}.`;
-}
-
-function pickComputerMove3() {
-    const randomNumber = Math.random();
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = 'Rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = 'Paper';
-    } else {
-        computerMove = 'Scissors';
-    }
-
-    let result = '';
-    if (computerMove === 'Rock') {
-        result = 'you lose';
-    } else if (computerMove === 'Paper') {
-        result = 'you win';
-    } else {
-        result = 'tie';
-    }
-
-    document.getElementById('result3').innerHTML = `You picked Scissors. Computer picked ${computerMove}. ${result}.`;
-}
-
-document.getElementById('rockBtn').addEventListener('click', pickComputerMove);
-document.getElementById('paperBtn').addEventListener('click', pickComputerMove2);
-document.getElementById('scissorsBtn').addEventListener('click', pickComputerMove3);
